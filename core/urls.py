0001b74fcbd1f3e461f581384aca8e282f335316
CHIRAG <chiragddayaramani@gmail.com>
2021-09-16 18:08:21 +0530

    created signup.html

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from . import views as core_views
 
 urlpatterns = [
     path('', core_views.home,name='home'),
+    path('signup/',core_views.signup,name='signup'),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    count=User.objects.count()
+    context={
+        'count':count,
+    }
+    return render(request,'home.html',context)
+
+def signup(request):
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form=  UserCreationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'registration/signup.html',context)
